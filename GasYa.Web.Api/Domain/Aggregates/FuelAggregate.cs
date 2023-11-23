abfa6889670a964b1a9f591494bee1a52bb045ca
Eduardo Chero <89673623+Ednoru@users.noreply.github.com>
2023-11-22 22:29:03 -0500

    Update FuelAggregate.cs

--- GasYa.Web.Api/Domain/Aggregates/FuelAggregate.cs
+++ GasYa.Web.Api/Domain/Aggregates/FuelAggregate.cs
@@ -9,21 +9,17 @@ public class FuelAggregate
 
     private FuelAggregate()
     {
-        // Constructor privado para garantizar que el agregado solo se cree a través de métodos de fábrica
     }
 
     public static FuelAggregate Create(string type, string description, double price)
     {
-        // Lógica de validación y creación del agregado
         var fuel = new FuelAggregate
         {
-            // Asigna propiedades y realiza validaciones según sea necesario
             Type = type,
             Description = description,
             Price = price
         };
 
-        // Puedes agregar más lógica de negocio o validaciones aquí
 
         return fuel;
     }
