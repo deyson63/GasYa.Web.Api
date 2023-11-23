25601ac37be55433ba4202d74083f27be4e0fc11
Eduardo Chero <89673623+Ednoru@users.noreply.github.com>
2023-11-22 22:29:59 -0500

    Update Password.cs

--- GasYa.Web.Api/Domain/ValueObjects/Password.cs
+++ GasYa.Web.Api/Domain/ValueObjects/Password.cs
@@ -11,7 +11,6 @@ public class Password
             throw new ArgumentException("El valor de la contraseña no puede estar vacío.", nameof(value));
         }
 
-        // Puedes agregar lógica adicional de validación de contraseña según tus necesidades
 
         _value = value;
     }
