a34da7c4c35d10fdc9eb3ebca64bc38f8610a161
Eduardo Chero <89673623+Ednoru@users.noreply.github.com>
2023-11-22 22:28:28 -0500

    Update UserService.cs

--- GasYa.Web.Api/Application/Implementations/UserService.cs
+++ GasYa.Web.Api/Application/Implementations/UserService.cs
@@ -7,7 +7,6 @@ namespace GasYa.Web.Api.Application.Implementations;
 
 public class UserService : IUserService
 {
-    // Puedes agregar más métodos según las operaciones relacionadas con usuarios
     public async Task<UserDto> GetAsync(GetUserQuery query)
     {
         return new UserDto
@@ -15,7 +14,6 @@ public class UserService : IUserService
             Id = 1,
             Name = "Ejemplo",
             Email = "ejemplo@example.com"
-            // Agrega más propiedades según tus necesidades
         };
     }
 
