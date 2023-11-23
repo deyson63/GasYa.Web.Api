516cbcd7aa728a211b854a89d28246028f97c039
Max Dayson Sabino Arostegui <94092599+deyson63@users.noreply.github.com>
2023-11-22 22:30:38 -0500

    Update PromotionController.cs

--- GasYa.Web.Api/Application/Controllers2/PromotionController.cs
+++ GasYa.Web.Api/Application/Controllers2/PromotionController.cs
@@ -52,6 +52,6 @@ namespace GasYa.Web.Api.Controllers
         public string Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        // Otros campos según sea necesario
+        
     }
 }
