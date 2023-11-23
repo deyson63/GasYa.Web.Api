a7e4aa5bdfac6e72110b2bf9bc5628e5e9c345a4
Eduardo Chero <89673623+Ednoru@users.noreply.github.com>
2023-11-22 22:29:14 -0500

    Update GasStationAggregate.cs

--- GasYa.Web.Api/Domain/Aggregates/GasStationAggregate.cs
+++ GasYa.Web.Api/Domain/Aggregates/GasStationAggregate.cs
@@ -7,19 +7,15 @@ public class GasStationAggregate
 
     private GasStationAggregate()
     {
-        // Constructor privado para garantizar que el agregado solo se cree a través de métodos de fábrica
     }
 
     public static GasStationAggregate Create(string name)
     {
-        // Lógica de validación y creación del agregado
         var gasStation = new GasStationAggregate
         {
-            // Asigna propiedades y realiza validaciones según sea necesario
             Name = name
         };
 
-        // Puedes agregar más lógica de negocio o validaciones aquí
 
         return gasStation;
     }
