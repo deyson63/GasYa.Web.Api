859542607499f7d29ef2ca2cef0ee9a60fa58209
Eduardo Chero <89673623+Ednoru@users.noreply.github.com>
2023-11-22 22:28:51 -0500

    Update ClientAggregate.cs

--- GasYa.Web.Api/Domain/Aggregates/ClientAggregate.cs
+++ GasYa.Web.Api/Domain/Aggregates/ClientAggregate.cs
@@ -8,24 +8,19 @@ public class ClientAggregate
     public Email Email { get; private set; }
     public Password Password { get; private set; }
 
-    // Otros miembros del agregado según las necesidades, por ejemplo, métodos de negocio
 
     private ClientAggregate()
     {
-        // Constructor privado para garantizar que el agregado solo se cree a través de métodos de fábrica
     }
 
     public static ClientAggregate Create(string firstName, string lastName, string email, string password)
     {
-        // Lógica de validación y creación del agregado
         var client = new ClientAggregate
         {
-            // Asigna propiedades y realiza validaciones según sea necesario
             Email = new Email(email),
             Password = new Password(password)
         };
 
-        // Puedes agregar más lógica de negocio o validaciones aquí
 
         return client;
     }
