e4525c9347507496438439926e963c5b59f5c75c
Eduardo Chero <89673623+Ednoru@users.noreply.github.com>
2023-11-22 22:28:19 -0500

    Update GasStationService.cs

--- GasYa.Web.Api/Application/Implementations/GasStationService.cs
+++ GasYa.Web.Api/Application/Implementations/GasStationService.cs
@@ -8,14 +8,10 @@ public class GasStationService : IGasStationService
 {
     public async Task<List<GasStationDto>> GasStationAsync(GetGasStationsQuery query)
     {
-        // Implementa la lógica para obtener las estaciones de servicio según el query
-        // Ejemplo: var gasStations = await gasStationRepository.GetGasStationsAsync(query);
-        // Mapea las entidades a DTOs y devuélvelos
         return new List<GasStationDto>
         {
             new GasStationDto { Id = 1, Name = "Estación 1" },
             new GasStationDto { Id = 2, Name = "Estación 2" }
-            // Agrega más estaciones según tus necesidades
         };
     }
 }
