2ea51badd3c9b61510dbb97ca3f51387d183f10c
Eduardo Chero <89673623+Ednoru@users.noreply.github.com>
2023-11-22 22:29:39 -0500

    Update PromotionAggregate.cs

--- GasYa.Web.Api/Domain/Aggregates/PromotionAggregate.cs
+++ GasYa.Web.Api/Domain/Aggregates/PromotionAggregate.cs
@@ -8,20 +8,16 @@ public class PromotionAggregate
 
     private PromotionAggregate()
     {
-        // Constructor privado para garantizar que el agregado solo se cree a través de métodos de fábrica
     }
 
     public static PromotionAggregate Create(string title, string description, DateTime startDate, DateTime endDate)
     {
-        // Lógica de validación y creación del agregado
         var promotion = new PromotionAggregate
         {
-            // Asigna propiedades y realiza validaciones según sea necesario
             Title = title,
             Description = description
         };
 
-        // Puedes agregar más lógica de negocio o validaciones aquí
 
         return promotion;
     }
